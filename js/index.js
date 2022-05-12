44697ab1517d27978bfaeed010ee2b8dc74e53cd
yahyaisfast <yahyaisfast@yahoo.com>
2022-05-11 23:59:35 -0700

    UPDATE
    -- fix more of the frontend
    -- have not gotten to backend yet
    -- will have to write the readme file afterwards

--- js/index.js
+++ js/index.js
@@ -11,4 +11,31 @@ function viewInventory(e){
 
     //retrieve the data from the server ajax call
 
+
+    //get ID from ajax call and set edit and save buttons to their values
+    document.getElementById('edit_Inventory').value = "111";
+    document.getElementById('save_Inventory').value = "111";
+}
+
+function editInventory(){
+    //save their current values under the innerhtml
+    var curValueQuantity = document.getElementById('inventory_quantity').innerHTML;
+    var curValueDescription = document.getElementById('inventory_description').innerHTML;
+    var curValueLocation = document.getElementById('inventory_location').innerHTML;
+
+    //change to inputs
+    document.getElementById('inventory_quantity').innerHTML = "<input class='form-control' value='" + curValueQuantity + "'>";
+    document.getElementById('inventory_description').innerHTML = "<textarea class='form-control'>" + curValueDescription + "</textarea>";
+    document.getElementById('inventory_location').innerHTML = "<input class='form-control' value='" + curValueLocation + "'>";
+
+    //hide edit button and show save button
+    document.getElementById('edit_Inventory').classList.toggle('d-none');
+    document.getElementById('save_Inventory').classList.toggle('d-none');
+}
+
+function saveInventory(){
+    //get the values from the inputs and undo edit inventory back to actual text
+    
+
+    //make ajax call
 }
