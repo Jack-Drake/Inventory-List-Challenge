fb08b4460871d64724d7291393969d5f6a7a9814
yahyaisfast <yahyaisfast@yahoo.com>
2022-05-13 11:53:15 -0700

    UPDATE

--- php/index.php
+++ php/index.php
@@ -1,16 +1,23 @@
 <?php
+class MyDB extends SQLite3
+{
+    function __construct()
+    {
+        $this->open('test.db');
+    }
+}
+
 //store origin payload prior to cleansing
 $payload_uncleansed = @file_get_contents('php://input');
 
 //check if the post data is present otherwise send to 404 not found
-if(!empty($payload_uncleansed)){
+if (!empty($payload_uncleansed)) {
     var_dump($payload_uncleansed);
 
     //switch statement for identifying which type of request is being made
-    
-}else{
+
+} else {
     //http response 404
     http_response_code(404);
     exit;
 }
-?>
